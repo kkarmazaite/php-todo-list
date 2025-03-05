2115e059aabdd68db0294cf4e5e85845bd24e5bb
Karolina Karmazaitė <karolina.karmazaite@cec.valantic.com>
2025-03-05 11:38:54 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    include("database.php");
+
+    include("header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>PHP todo list</title>
+</head>
+<body>
+    <main>
+        <div class="container">
+            hue
+        </div>
+    </main>
+</body>
+</html>
+
+<?php
+    include("footer.html");
+
+    mysqli_close($conn);
+?>
