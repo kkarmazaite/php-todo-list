d9e9bef1752dc64f3e54a5a98e198a7c41c03ff9
Karolina Karmazaitė <karolina.karmazaite@cec.valantic.com>
2025-03-06 11:27:54 +0200

    Adding registration form

--- index.php
+++ index.php
@@ -1,28 +1,7 @@
 <?php
     include("database.php");
 
-    include("header.html");
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>PHP todo list</title>
-</head>
-<body>
-    <main>
-        <div class="container">
-            hue
-        </div>
-    </main>
-</body>
-</html>
-
-<?php
-    include("footer.html");
+    header('Location: registration.php');
 
     mysqli_close($conn);
 ?>
---
+++ registration.php
@@ -0,0 +1,60 @@
+<?php
+    include("database.php");
+
+    include("header.html");
+
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if(empty($username) || empty($email) || empty( $password)) {
+            $error = "This email is already taken";
+        } else {
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO users (name, email, password)
+                    VALUES('$username', '$email','$hash')";
+
+            try {
+                mysqli_query($conn, $sql);
+                echo "You are now registered!";
+            } catch(mysqli_sql_exception) {
+                $error = "This email is already taken";
+            }
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>PHP todo list</title>
+</head>
+<body>
+    <main>
+        <div class="container">
+            <div class="card">
+                <form action="<?php htmlspecialchars( $_SERVER["PHP_SELF"] ) ?>" method="post">
+                    <h2>Registration</h2>
+                    <label>username:</label><br>
+                    <input type="text" name="username" placeholder="name"><br>
+                    <label>email:</label><br>
+                    <input type="email" name="email" placeholder="name@test.com"><br>
+                    <label>password:</label><br>
+                    <input type="password" name="password" placeholder="password"><br><br>
+                    <input class="button" type="submit" name="submit" value="register"><br>
+                </form>
+                
+                <div class="error-message"><?php echo (isset($error))?$error:'';?></div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
+
+<?php
+    include("footer.html");
+?>
